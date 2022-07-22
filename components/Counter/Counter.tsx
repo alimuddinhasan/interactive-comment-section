81a6f8ea0e2c96f13691f17821fd7a49edd0056f
Alimuddin Hasan <mt-alimuddin.hasanalkabir@tech.jago.com>
2022-07-22 16:43:00 +0700

    feat(counter): add button handler

--- components/Counter/Counter.tsx
+++ components/Counter/Counter.tsx
@@ -1,9 +1,7 @@
 import Image from "next/image";
 import React from "react";
-import iconPlus from "assets/icon-plus.svg";
-import iconMinus from "assets/icon-minus.svg";
 import Button from "../Button/Button";
-import { Icons } from '../../constant/icon.constant';
+import { Icons } from "../../constant/icon.constant";
 
 interface ICounterProps {
   value: number;
@@ -18,9 +16,13 @@ export default function Counter({
 }: ICounterProps) {
   return (
     <div className='flex justify-between items-center gap-3 bg-light-gray py-2 px-4 rounded-xl'>
-      <Button icon={Icons.plus} dataTestId="button-plus" />
+      <Button icon={Icons.plus} dataTestId='button-plus' onClick={onIncrease} />
       <p className='text-moderate-blue'>{value}</p>
-      <Button icon={Icons.minus} dataTestId="button-minus" />
+      <Button
+        icon={Icons.minus}
+        dataTestId='button-minus'
+        onClick={onDecrease}
+      />
     </div>
   );
 }
